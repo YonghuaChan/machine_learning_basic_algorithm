a9c7f4bd20d5d15486f9d465c4c182ac5967eaec
YonghuaChan <39548880+YonghuaChan@users.noreply.github.com>
2018-11-26 18:01:45 +0800

    2018/11/26 更新

---
+++ doc/3-jupyter notebook&Numpy/mymodule/FirstML.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Author: YonghuaChan
+@File  : FirstML.py
+@Time  : 2018/11/17 13:03
+@Desc  : 
+'''
+
+
+def predict(x):
+    print("?")
---
+++ doc/3-jupyter notebook&Numpy/mymodule/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Author: YonghuaChan
+@File  : __init__.py
+@Time  : 2018/11/17 13:02
+@Desc  : 
+'''
